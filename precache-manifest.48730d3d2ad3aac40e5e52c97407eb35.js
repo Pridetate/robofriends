baf94e16d658e819c37dac10aad0a7c87dd94add
Pridetate <pmuchirawetu@gmail.com>
2020-08-06 03:45:43 +0200

    Updates

---
+++ precache-manifest.48730d3d2ad3aac40e5e52c97407eb35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975076d4691b1a838d8e8e064a3f393b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "bd8e0efdd93d7f0fb89f",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e06d5bbce3ef2fa5fd12",
+    "url": "/robofriends/static/css/main.c7c43201.chunk.css"
+  },
+  {
+    "revision": "bd8e0efdd93d7f0fb89f",
+    "url": "/robofriends/static/js/2.a36a8a4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a36a8a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06d5bbce3ef2fa5fd12",
+    "url": "/robofriends/static/js/main.1e026a41.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
